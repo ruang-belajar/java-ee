58f2af7997c4e10e132a647a0b07ec1e67313175
pujangga123 <rush_x@yahoo.com>
2024-05-06 22:42:19 +0700

    2024-05-06

--- docs/faq.md
+++ docs/faq.md
@@ -8,3 +8,41 @@ Hapus koneksi Server dari Netbeans ke Tomcat, kemudian buat ulang untuk memaksa 
 2. Pilih _Apache Tomcat or TomEE_
 3. klik _Remove Server_
 4. Buat kembali koneksi server, klik _Add Server_. Check _Setup koneksi ke server dari Netbeans_ di [Panduan Setup](setup.md)
+
+---
+
+**Masalah:** _javax.servlet.ServletException: java.lang.UnsupportedClassVersionError: `......` has been compiled by a more recent version of the Java Runtime class file version 61.0), this version of the Java Runtime only recognizes class file versions up to 52.0 (unable to load class [`......`])_
+
+**Solusi:** Turunkan versi JDK. Caranya:
+1. Persiapan:
+   - Klik menu: _Tools - Options_
+   - Halaman _Miscelaneous_, tab _Files_
+   - _Uncheck_ opsi _Enable auto-scanning of sources_
+   - Klik _OK_
+2. Downgrade JDK version:
+   - Klik menu: _File - Project Properties_
+   - Halaman _Sources_, rubah _Source/Binary Format_ ke versi lebih bawah.
+
+**Tabel versi JDK**
+| Java SE (JDK)	| Major Version	|
+| --- | --- |
+| 17	| 61	|
+| 16	| 60	|
+| 15	| 59	|
+| 14	| 58	|
+| 13	| 57	|
+| 12	| 56	|
+| 11	| 55	|
+| 10	| 54	|
+| 9	| 53	|
+| 8	| 52	|
+| 7	| 51	|
+| 6	| 50	|
+| 5.0	| 49	|
+| 1.4	| 48	|
+| 1.3	| 47	|
+| 1.2	| 46	|
+| 1.1	| 45	|
+| 1.01	| 45	|
+
+---
---
+++ src/Mahasiswa1.java
@@ -0,0 +1,116 @@
+/*
+    berikut adalah contoh penggunaan class untuk keperluan baca/tulis
+    ke dabatase.
+*/
+
+package kampus;  // rubah nama paket sesuai dengan paket yang Anda miliki
+
+import java.sql.*;
+
+public class Mahasiswa1 {
+    public String nim;
+    public String nama;
+    public Double ipk;
+
+    public Mahasiswa() {
+        this.nim = "";
+        this.nama = "";
+        this.ipk = 0.0;
+    }
+
+    public void baca(String nim) {
+        // deklarasi variable yang bisa diakses di seluruh halaman
+        Connection connection = null;
+        ResultSet rs = null;
+
+        try {
+            String connectionURL = "jdbc:mysql://localhost/test";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionURL, "root", "");
+
+            if (!connection.isClosed()) {
+                // prepare select statement
+                String sql = "SELECT * FROM mahasiswa where nim=?";
+                PreparedStatement st = connection.prepareStatement(sql);
+                st.setString(1, nim);
+                rs = st.executeQuery();
+                
+                if(rs.next()) {
+                    this.nim = rs.getString("nim");
+                    this.nama = rs.getString("nama");
+                    this.ipk = rs.getDouble("ipk");
+                }
+                
+            }
+        } catch (Exception ex) {
+            System.out.println("Error "+ex.toString());
+        }
+    }
+
+    public void tambah() {
+        // deklarasi variable yang bisa diakses di seluruh halaman
+        Connection connection = null;
+
+        try {
+            String connectionURL = "jdbc:mysql://localhost/test";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionURL, "root", "");
+
+            if (!connection.isClosed()) {
+                // prepare select statement
+                String sql = "INSERT INTO mahasiswa (nim,nama,ipk) values (?,?,?)";
+                PreparedStatement st = connection.prepareStatement(sql);
+                st.setString(1, this.nim);
+                st.setString(2, this.nama);
+                st.setDouble(3, this.ipk);
+                st.executeUpdate();
+            }
+            connection.close();
+        } catch (Exception ex) {
+            //
+        }
+    }
+
+    // melakukan data dengan cara seperti tambah() dan baca(), memiliki redudansi
+    // kode untuk membuat koneksi. Hal ini tidak efisien.
+    // Masalah lain adalah, jika ada perubahan konfigurasi database (username/
+    // password/database) maka perubahan harus dilakukan di banyak bagian.
+    // Untuk mengatasi masalah ini, kita membuat class DatabaseTest, sehingga
+    // setting dan perubahan konfigurasi database dilakukan di satu tempat.
+    public void hapus() {
+        Connection connection = null;
+        try {
+            connection = DatabaseTest.connect();
+
+            if (!connection.isClosed()) {
+                // prepare select statement
+                String sql = "DELETE FROM mahasiswa WHERE nim=?";
+                PreparedStatement st = connection.prepareStatement(sql);
+                st.setString(1, this.nim);
+                st.executeUpdate();
+            }
+            connection.close();
+        } catch (Exception ex) {
+        }
+    }
+    
+    public void update() {
+        Connection connection = null;
+        try {
+            connection = DatabaseTest.connect();
+
+            if (!connection.isClosed()) {
+                // prepare select statement
+                String sql = "UPDATE mahasiswa SET nama=?, ipk=? WHERE nim=?";
+                PreparedStatement st = connection.prepareStatement(sql);
+                st.setString(1, this.nama);                
+                st.setDouble(2, this.ipk);                
+                st.setString(3, this.nim);
+
+                st.executeUpdate();
+            }
+            connection.close();
+        } catch (Exception ex) {
+        }
+    }
+}
